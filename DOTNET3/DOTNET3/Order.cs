bd1983870bf1fc48bd01ee1d51dfd07fb9a89591
JXfl27@outlook.com <JXfl27@outlook.com>
2020-09-28 07:42:29 +0800

    1

---
+++ DOTNET3/DOTNET3/Order.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace DOTNET3
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public List<OrderDetails> ods { get; set; }
+        public Order()
+        {
+            ods = new List<OrderDetails>();
+        }
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null || obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                var order = obj as Order;
+                if (order.ID != this.ID || order.name != this.name)
+                {
+                    return false;
+                }
+                return order.ods == this.ods;
+            }
+           
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID.GetHashCode(), name.GetHashCode(), ods.GetHashCode());
+        }
+
+        public int CompareTo(object x)
+        {
+            if (ID == ((Order)x).ID)
+            {
+                return 0;
+            }
+            return ID < ((Order)x).ID ? -1 : 1;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+"ID:"+ID+"CustomerName:"+name+ods.ToString();
+        }
+    }
+}
